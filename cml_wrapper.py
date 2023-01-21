4fc9f2cb6170d3c8d266c6e085ddc64ad35a03a3
uerdemki <uerdemk@hotmail.com>
2023-01-21 23:46:03 +0300

    deneme

--- cml_wrapper.py
+++ cml_wrapper.py
@@ -20,9 +20,15 @@ parser.add_argument('--topology', type=str, help='CML topology pass (default is 
 parser.add_argument('--action', type=str, help='create or destroy (default create)')
 args = parser.parse_args()
 
-f1= open("topology_file.yaml","a")
+f1= open("./topology_file.yaml", "w")
 f1.write(json.dumps(parse_config('./cml_ci_topology.yaml')))
 f1.close()
+
+f = open('/topology_file.yaml', 'r')
+content = f. read()
+print(content)
+f. close()
+
 
 
 if args.url == None:
